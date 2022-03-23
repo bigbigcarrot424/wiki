58b024d729ddb0050dab1135045c47d8baa24a77
bigbigcarrot424 <941389381@qq.com>
2022-03-23 22:27:38 +0800

    2-7 add hello 接口测试

--- src/main/java/com/fangshuo/wiki/controller/TestController.java
+++ src/main/java/com/fangshuo/wiki/controller/TestController.java
@@ -1,12 +1,18 @@
 package com.fangshuo.wiki.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-    @RequestMapping("/hello")
+    @GetMapping("/hello")
     public String hello(){
         return "hello world";
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name){
+        return "hello world! Post" + name;
     }
 }
