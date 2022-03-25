99674ee86ddaf07aa5bf3026eda0df2596b0352d
bigbigcarrot424 <941389381@qq.com>
2022-03-25 12:08:00 +0800

    3-8 add CopyUtil 封装 BeanUtils

--- src/main/java/com/fangshuo/wiki/service/EbookService.java
+++ src/main/java/com/fangshuo/wiki/service/EbookService.java
@@ -1,7 +1,11 @@
 package com.fangshuo.wiki.service;
 
 import com.fangshuo.wiki.domain.Ebook;
+import com.fangshuo.wiki.domain.EbookExample;
 import com.fangshuo.wiki.mapper.EbookMapper;
+import com.fangshuo.wiki.req.EbookReq;
+import com.fangshuo.wiki.resp.EbookResp;
+import com.fangshuo.wiki.util.CopyUtil;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -12,7 +16,18 @@ public class EbookService {
     @Resource
     private EbookMapper ebookMapper;
 
-    public List<Ebook> list(){
-        return ebookMapper.selectByExample(null);
+    public List<EbookResp> list(EbookReq req){
+        EbookExample ebookExample = new EbookExample();
+        EbookExample.Criteria criteria = ebookExample.createCriteria();
+        criteria.andNameLike("%" + req.getName() + "%");
+        List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+        /*List<EbookResp> respList = new ArrayList<>();
+        for (Ebook ebook : ebookList) {
+            EbookResp ebookResp = new EbookResp();
+            BeanUtils.copyProperties(ebook, ebookResp);
+            respList.add(ebookResp);
+        }*/
+        List<EbookResp> respList = CopyUtil.copyList(ebookList, EbookResp.class);
+        return respList;
     }
 }
