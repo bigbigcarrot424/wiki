ff73bbd0d81e5c494cb82193d107d3d18c614c5a
bigbigcarrot424 <941389381@qq.com>
2022-03-25 21:55:55 +0800

    5-8 增加拦截器，打印接口耗时

---
+++ src/main/java/com/fangshuo/wiki/config/SpringMvcConfig.java
@@ -0,0 +1,23 @@
+package com.fangshuo.wiki.config;
+
+import com.fangshuo.wiki.interceptor.LogInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+    @Resource
+    LogInterceptor loginInterceptor;
+
+
+
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/**").excludePathPatterns("/login");
+
+    }
+}
