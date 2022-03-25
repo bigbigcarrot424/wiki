61a5c4ce1e49b822e76e197eb5f17d1db9451c21
bigbigcarrot424 <941389381@qq.com>
2022-03-25 18:31:36 +0800

    4-7 制作the-footer组件

---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,12 @@
+<template>
+    <a-layout-footer style="text-align: center">
+        Ant Design ©2018 Created by Fang Shuo
+    </a-layout-footer>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from 'vue';
+    export default defineComponent({
+        name: 'the-footer',
+    });
+</script>
