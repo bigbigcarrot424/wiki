5982a4d7e92a12b44a542b8f25808d04e5d176e9
bigbigcarrot424 <941389381@qq.com>
2022-04-02 11:53:48 +0800

    8-12 使用树形组件展示文档树 遗留BUG，文档树应该只查当前电子书下所有的文档

--- web/src/views/doc.vue
+++ web/src/views/doc.vue
@@ -1,9 +1,73 @@
 <template>
     <a-layout>
         <a-layout-content :style="{background: '#fff', padding: '24px', margin: 0, minHeight: '280px'}">
+            <a-row>
+                <a-col :span="6">
+                    <a-tree
+                        v-if="level1&&level1.length > 0"
+                        :tree-data="level1"
+                        @select="onSelect"
+                        :replaceFields="{title: 'name', key: 'id', value: 'id'}"
+                        :defaultExpandAllRows="true"
+                    >
+                    </a-tree>
+                </a-col>
+                <a-col :span="18">
+
+                </a-col>
+            </a-row>
             <div class="div">
                 <h1>欢迎来到文档页面</h1>
             </div>
         </a-layout-content>
     </a-layout>
 </template>
+
+<script lang="ts">
+    import { defineComponent, onMounted, ref, createVNode } from 'vue';
+    import axios from 'axios';
+    import {message, Modal} from 'ant-design-vue';
+    import {Tool} from "@/util/tool";
+    import {useRoute} from "vue-router";
+    import {ExclamationCircleOutlined} from "@ant-design/icons-vue/lib";
+    import E from 'wangeditor'
+
+
+    export default defineComponent({
+        name: 'Doc',
+        setup() {
+            const route = useRoute();
+            const docs = ref();
+            const level1 = ref();
+            level1.value = [];
+
+
+            /**
+             * 数据查询
+             **/
+            const handleQuery = () => {
+                    axios.get("/doc/all").then((response) => {
+                        const data = response.data;
+                        if (data.success){
+                            docs.value = data.content;
+                            console.log("原始数据", docs.value);
+                            level1.value = [];
+                            level1.value = Tool.array2Tree(docs.value, 0);
+                            console.log("树形结构", level1.value);
+                        }else {
+                            message.error(data.message);
+                        }
+                    })
+                };
+
+            onMounted(() => {
+                handleQuery();
+            });
+
+            return {
+                handleQuery,
+                level1,
+            }
+        }
+    });
+</script>
