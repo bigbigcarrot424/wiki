32d5280735918dbce009ee78a773751d9fddb909
bigbigcarrot424 <941389381@qq.com>
2022-04-08 20:51:45 +0800

    9-8 登录功能前端界面开发

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -21,13 +21,59 @@
             <a-menu-item key="/about">
                 <router-link to="/about">关于我们</router-link>
             </a-menu-item>
+            <a class="login-menu" @click="showLoginModal">
+                <span>登录</span>
+            </a>
         </a-menu>
+        <a-modal v-model:visible="loginModalVisible" title="登录" @ok="login" :confirm-loading="loginModalLoading">
+            <a-form :model="loginUser" :label-col="labelCol" :wrapper-col="wrapperCol">
+                <a-form-item label="登录名">
+                    <a-input v-model:value="loginUser.loginName" :disabled="!!user.id"/>
+                </a-form-item>
+                <a-form-item label="密码">
+                    <a-input v-model:value="loginUser.password" type="password"/>
+                </a-form-item>
+            </a-form>
+        </a-modal>
     </a-layout-header>
 </template>
 
 <script lang="ts">
-    import { defineComponent } from 'vue';
+    import { defineComponent, ref } from 'vue';
+    import axios from "axios";
     export default defineComponent({
         name: 'the-header',
+        setup(){
+            const loginUser = ref({
+                loginName: "test",
+                password: "test",
+            })
+            let loginModalVisible = ref(false);
+            let loginModalLoading = ref(false);
+            const showLoginModal = () => {
+                loginModalVisible.value = true;
+            }
+
+            const login = () => {
+                loginModalLoading.value = true;
+                axios.post("/user/login",loginUser.value).then((response) => {
+                    loginModalLoading.value = false;
+
+                })
+            }
+            return {
+                loginModalVisible,
+                loginModalLoading,
+                showLoginModal,
+                loginUser,
+                login,
+            }
+        }
     });
 </script>
+<style>
+    .login-menu {
+        float: right;
+        color: white;
+    }
+</style>
