2a9be716f8041f04e243a20d7acca2780e4b7913
bigbigcarrot424 <941389381@qq.com>
2022-04-09 13:39:33 +0800

    9-12 增加路由登录拦截，未登录时跳到首页

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -7,6 +7,8 @@ import AdminUser from '../views/admin/admin-user.vue'
 import AdminEbook from '../views/admin/admin-ebook.vue'
 import AdminCategory from '../views/admin/admin-category.vue'
 import AdminDoc from '../views/admin/admin-doc.vue'
+import store from "@/store";
+import {Tool} from "@/util/tool";
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -22,22 +24,34 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/admin/ebook',
     name: 'AdminEbook',
-    component: AdminEbook
+    component: AdminEbook,
+    meta: {
+      loginRequire: true
+    }
   },
   {
     path: '/admin/category',
     name: 'AdminCategory',
-    component: AdminCategory
+    component: AdminCategory,
+    meta: {
+      loginRequire: true
+    }
   },
   {
     path: '/admin/doc',
     name: 'AdminDoc',
-    component: AdminDoc
+    component: AdminDoc,
+    meta: {
+      loginRequire: true
+    }
   },
   {
     path: '/admin/user',
     name: 'AdminUser',
-    component: AdminUser
+    component: AdminUser,
+    meta: {
+      loginRequire: true
+    }
   },
   {
     path: '/doc',
@@ -51,4 +65,21 @@ const router = createRouter({
   routes
 })
 
+//路由登录拦截
+router.beforeEach((to, from, next) => {
+  if(to.matched.some(function (item) {
+    console.log(item, "是否需要登录校验 : ", item.meta.loginRequire);
+    return item.meta.loginRequire;
+  })){
+    const loginUser = store.state.user;
+    if (Tool.isEmpty(loginUser)) {
+      console.log("用户未登录！")
+      next("/");
+    }else {
+      next();
+    }
+  }else {
+    next();
+  }
+});
 export default router
