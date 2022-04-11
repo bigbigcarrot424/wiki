afdd60172e164cab7034956db27d655871d2aeb8
bigbigcarrot424 <941389381@qq.com>
2022-04-11 22:52:43 +0800

    10-7 增加日志流水号，方便生产运维

--- src/main/java/com/fangshuo/wiki/job/DocJob.java
+++ src/main/java/com/fangshuo/wiki/job/DocJob.java
@@ -1,8 +1,10 @@
 package com.fangshuo.wiki.job;// package com.jiawa.wiki.job;
 
 import com.fangshuo.wiki.service.DocService;
+import com.fangshuo.wiki.util.SnowFlake;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -13,6 +15,10 @@ public class DocJob {
     @Resource
     private DocService docService;
 
+    @Resource
+    private SnowFlake snowFlake;
+
+
     private static final Logger LOG = LoggerFactory.getLogger(DocJob.class);
 
    /**
@@ -20,6 +26,8 @@ public class DocJob {
     */
    @Scheduled(cron = "0/30 * * * * ?")
    public void cron() throws InterruptedException {
+       // 增加日志流水号
+       MDC.put("LOG_ID", String.valueOf(snowFlake.nextId()));
        LOG.info("更新电子书下的文档数据开始");
        long start = System.currentTimeMillis();
        docService.updateEbookInfo();
